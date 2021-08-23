60a1947277a8ec1b355ecb6ac10f0887684ce600
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2021-08-23 12:28:38 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Welcome to the best RPG Ever! Before we begin, we need to start out with the basic necessities")
+username = input("What would you like your username to be?")
+print(f"Cool name {username}!")
+print("Now we need to choose your role")
+choice = input(f"Please choose either role: Hero or Prince")
+print("Great!")
+print(f"Now that we have the necessities out of they way. It is time to begin {username}")
