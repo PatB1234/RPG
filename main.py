38db0800980daebb4593fe2f5275187257834e9c
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2021-09-19 11:37:04 +0300

    Wrote a help command and a quit command

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
+import time, sys
 Damage = 0
 Sword = "Wooden Sword "
 Armour = "Chainmail Armour "
 Protection = 5
 inventory = [(str(Sword) + "Damage: " + str(Damage)), (str(Armour) + "Protection: " + str(Protection))] 
+commandList = [".inv", ".help", ".quit"]
 def commands(action):
 
     if (action == ".inv"):
@@ -12,6 +13,16 @@ def commands(action):
 
             print(inventory[a])
 
+    if (action == ".help"):
+
+        for b in range(len(commandList)):
+
+            print(commandList[b])
+
+    if (action == ".quit"):
+
+        print("See ya later!")    
+        sys.exit()
 print("Welcome to the best RPG Ever! Before we begin, we need to start out with the basic necessities")
 username = input("What would you like your username to be?")
 print(f"Cool name {username}!")
@@ -25,7 +36,7 @@ if choice == "Hero" or choice == "hero":
     time.sleep(1)
     print("Zzzzzz....Ugh")
     time.sleep(0.5)
-    print("You wake up with the worst headache for the first time in years. You quickly puit on your gear and report to the king. If you would like to see your inventory, type .inv At a given prompt like this one!")
+    print("You wake up with the worst headache for the first time in years. You quickly put on your gear and report to the king. If you would like to see your inventory, type .inv for your inventory or .help for a list of commands at a given prompt like this one!")
     time.sleep(3)
     action = input("What would you like to do")
     commands(action)
