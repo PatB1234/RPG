a6baf110b8a89cc16eb835dcdb447fc822d38728
PatB1234 <80209204+PatB1234@users.noreply.github.com>
2021-09-21 17:59:37 +0300

    Made some enemy code

---
+++ enemyScript.py
@@ -0,0 +1,13 @@
+import time
+from main import *
+
+while True:
+
+    if (enemy):
+
+        print(f"The enemy is a {enemyType}")
+        print(f"The enemy health is {enemyHealth}")
+
+    else:
+
+        pass
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import time, sys
+import enemyScript
+
+global enemy
+global enemyHealth
+global enemyType
+
+enemyType = "Goblin"
+enemy = False
+enemyHealth = 0
 Damage = 0
 Sword = "Wooden Sword "
 Armour = "Chainmail Armour "
 Protection = 5
 inventory = [(str(Sword) + "Damage: " + str(Damage)), (str(Armour) + "Protection: " + str(Protection))] 
 commandList = [".inv", ".help", ".quit"]
+
 def commands(action):
 
     if (action == ".inv"):
@@ -19,10 +29,25 @@ def commands(action):
 
             print(commandList[b])
 
+    if (action == ".attk"):
+
+        if (enemy):
+
+            pass
+        
+        else:
+
+            print("There is not enemy near by")
+
     if (action == ".quit"):
 
         print("See ya later!")    
         sys.exit()
+def doAction():
+
+    action = input("What would you like to do")
+    commands(action)
+
 print("Welcome to the best RPG Ever! Before we begin, we need to start out with the basic necessities")
 username = input("What would you like your username to be?")
 print(f"Cool name {username}!")
@@ -38,5 +63,9 @@ if choice == "Hero" or choice == "hero":
     time.sleep(0.5)
     print("You wake up with the worst headache for the first time in years. You quickly put on your gear and report to the king. If you would like to see your inventory, type .inv for your inventory or .help for a list of commands at a given prompt like this one!")
     time.sleep(3)
-    action = input("What would you like to do")
-    commands(action)
+    doAction()
+    print("You have now learnt the basics of using this game. You .help menu will increase as you unlock new commands.")
+    time.sleep(3)
+    commandList.append(".attk")
+    doAction()
+    enemy = True
